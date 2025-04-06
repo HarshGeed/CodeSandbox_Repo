63b5cd291c385e97e4c3cc2e4eadf4c9af0c521b
HarshGeed <85499874+HarshGeed@users.noreply.github.com>
2025-04-06 09:19:03 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>This is Pinned to the taskbar for testing our project ideas</h1>
+    </div>
+  );
+}
